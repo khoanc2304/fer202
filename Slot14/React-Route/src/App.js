e98c68e2e6de0a3e7017a3ffc7709fdb7f206ff0
khoanc2304 <1fvhtkhoa@gmail.com>
2025-06-27 22:34:50 +0700

    Add react-route...

---
+++ Slot14/React-Route/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navigation from './components/Navigation';
+import Post from './components/Post';
+import PostDetail from './components/PostDetail';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Post />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
